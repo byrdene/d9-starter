0d40cf002b834d38215626d7c3a262436a477498
Suzanne Strahl-Cooper <>
2021-05-30 18:07:40 -0400

    Another round of base install config; Update settings with tmp and private path

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -529,7 +529,7 @@ $settings['update_free_access'] = FALSE;
  * See https://www.drupal.org/documentation/modules/file for more information
  * about securing private files.
  */
-# $settings['file_private_path'] = '';
+$settings['file_private_path'] = 'sites/default/files/private';
 
 /**
  * Temporary file path:
@@ -542,7 +542,7 @@ $settings['update_free_access'] = FALSE;
  *
  * @see \Drupal\Component\FileSystem\FileSystem::getOsTemporaryDirectory()
  */
-# $settings['file_temp_path'] = '/tmp';
+$settings["file_temp_path"] = $_SERVER['HOME'] . '/tmp';
 
 /**
  * Session write interval:
